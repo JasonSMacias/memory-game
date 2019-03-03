44d6b7516fe48cf570a445cd80fce834ccb925f4
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-03-02 23:39:10 -0500

    Set up index.html with bulma, set up general layout in app.js to be abstracted into components later

--- src/App.js
+++ src/App.js
@@ -5,21 +5,106 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="columns is-mobile">
+          <div className="column is-2 is-offset-1">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+        </div>
+
+        <div className="columns is-mobile">
+          <div className="column is-2 is-offset-1">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+        </div>
+
+        <div className="columns is-mobile is-vcentered">
+          <div className="column is-2 is-offset-1">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-6">
+            <div className="is-size-5-mobile is-size-4-tablet is-size-2-desktop has-text-centered">Click an icon, don't repeat!</div>
+          </div>
+
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+        </div>
+
+          <div className="columns is-vcentered is-mobile">
+          <div className="column is-2 is-offset-1">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-6">
+            <div className="columns is-mobile">
+              <div className="column is-size-5-mobile is-size-3-tablet is-size-1-desktop has-text-right">Score: <span className="has-text-danger">0</span></div>
+              <div className="column is-size-5-mobile is-size-3-tablet is-size-1-desktop has-text-success">Best: <span className="has-text-danger">0</span></div>
+            </div>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+        </div>
+
+        <div className="columns is-mobile">
+          <div className="column is-2 is-offset-1">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+        </div>
+
+                <div className="columns is-mobile">
+          <div className="column is-2 is-offset-1">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+          <div className="column is-2">
+            <button className="button is-large is-success is-size-3-tablet is-size-2-desktop"><i className="fas fa-pepper-hot"></i></button>
+          </div>
+        </div>
       </div>
     );
   }
