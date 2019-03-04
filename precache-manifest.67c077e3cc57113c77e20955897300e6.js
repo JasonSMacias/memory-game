922b827c861e0e4929b7fb20c04b95ca2c520830
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-03-03 23:19:11 -0500

    Updates

---
+++ precache-manifest.67c077e3cc57113c77e20955897300e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a28a5e4b2ef3343732d",
+    "url": "/JasonSMacias/memory-game/static/js/runtime~main.4a28a5e4.js"
+  },
+  {
+    "revision": "2df85bde5f2317c8642d",
+    "url": "/JasonSMacias/memory-game/static/js/main.2df85bde.chunk.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/JasonSMacias/memory-game/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "2df85bde5f2317c8642d",
+    "url": "/JasonSMacias/memory-game/static/css/main.ac6f1749.chunk.css"
+  },
+  {
+    "revision": "1a5cf7e58531b629f3a6955dd0597fbd",
+    "url": "/JasonSMacias/memory-game/index.html"
+  }
+];
